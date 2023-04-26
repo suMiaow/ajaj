5d09005cb574c9642450994aa50d1259db9b1b54
chen.xi <chen.xi@gtechdigi.com>
2023-04-26 18:47:34 +0800

    04261847

--- src/main/java/com/meme/AjApplication.java
+++ src/main/java/com/meme/AjApplication.java
@@ -1,15 +1,27 @@
 package com.meme;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.io.Resource;
 
+import java.io.IOException;
+import java.util.Locale;
+
+@Slf4j
 @SpringBootApplication
 public class AjApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ConfigurableApplicationContext context = SpringApplication.run(AjApplication.class);
 
+        log.info(context.getMessage("hi", null, Locale.SIMPLIFIED_CHINESE));
+        log.info(context.getMessage("hi", null, Locale.ENGLISH));
+        log.info(context.getMessage("hi", null, Locale.JAPANESE));
 
-        System.out.println(context);
+        Resource[] resources = context.getResources("classpath:application.yml");
+        Resource[] springFactories = context.getResources("classpath*:META-INF/spring.factories");
+
+        log.info("{}", springFactories);
 
     }
 }
