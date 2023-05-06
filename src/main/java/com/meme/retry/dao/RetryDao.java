3fb3acf55bea1977fee52216ae7efc3b9ebc334e
chen.xi <chen.xi@gtechdigi.com>
2023-05-06 18:06:26 +0800

    05061806

--- src/main/java/com/meme/retry/dao/RetryDao.java
+++ src/main/java/com/meme/retry/dao/RetryDao.java
@@ -1,16 +1,13 @@
 package com.meme.retry.dao;
 
 import com.meme.retry.model.RetryInfo;
-import lombok.SneakyThrows;
 
 import java.util.List;
 
 public interface RetryDao {
 
-    @SneakyThrows
     void save(RetryInfo retryInfo);
 
-    @SneakyThrows
     List<RetryInfo> peek();
 
     void delete(RetryInfo retryInfo);
