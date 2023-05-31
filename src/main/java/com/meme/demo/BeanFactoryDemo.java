bdf6a535a26b1cfddaa3791fdf60d1e8434770ee
chen.xi <chen.xi@gtechdigi.com>
2023-05-31 16:19:54 +0800

    05311619

---
+++ src/main/java/com/meme/demo/BeanFactoryDemo.java
@@ -0,0 +1,77 @@
+package com.meme.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.annotation.AnnotationConfigUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+@Slf4j
+public class BeanFactoryDemo {
+
+    public static void main(String[] args) {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        AbstractBeanDefinition configBeanDefinition = BeanDefinitionBuilder.genericBeanDefinition(BeanConfig.class).setScope("singleton").getBeanDefinition();
+        beanFactory.registerBeanDefinition("config", configBeanDefinition);
+
+
+        AnnotationConfigUtils.registerAnnotationConfigProcessors(beanFactory);
+
+        Map<String, BeanFactoryPostProcessor> beanFactoryPostProcessors = beanFactory.getBeansOfType(BeanFactoryPostProcessor.class);
+        for (BeanFactoryPostProcessor beanFactoryPostProcessor : beanFactoryPostProcessors.values()) {
+            beanFactoryPostProcessor.postProcessBeanFactory(beanFactory);
+        }
+        Map<String, BeanPostProcessor> beanPostProcessors = beanFactory.getBeansOfType(BeanPostProcessor.class);
+        beanFactory.addBeanPostProcessors(beanPostProcessors.values());
+
+        beanFactory.preInstantiateSingletons();
+
+        String[] beanDefinitionNames = beanFactory.getBeanDefinitionNames();
+
+        for (String beanDefinitionName : beanDefinitionNames) {
+            log.info(beanDefinitionName);
+        }
+
+        log.info("bean1.bean2: {}", ((Bean1) beanFactory.getBean("bean1")).bean2);
+    }
+
+    @Configuration
+    public static class BeanConfig {
+        @Bean
+        public Bean1 bean1() {
+            return new Bean1();
+        }
+
+        @Bean
+        public Bean2 bean2() {
+            return new Bean2();
+        }
+
+    }
+
+    public static class Bean1 {
+
+        public Bean1() {
+            log.info("construct bean1");
+        }
+
+        @Autowired
+        private Bean2 bean2;
+
+    }
+
+
+    public static class Bean2 {
+
+        public Bean2() {
+            log.info("construct bean2");
+        }
+    }
+}
